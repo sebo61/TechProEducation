be4df75369731b46f23ac87e468382cdf0c37a1c
Sebahattin KÖSE <sebo61@gmail.com>
2023-11-27 21:03:37 +0300

    251123

---
+++ FrontendDeveloper/javascript/day11/js/49.1-script.js
@@ -0,0 +1,9 @@
+
+(function(){
+let name="Ali";
+
+const topla = (a,b) => a+b;
+console.log(topla(3,5))
+alert(name)
+
+})()
---
+++ FrontendDeveloper/javascript/day11/js/49.2-script.js
@@ -0,0 +1,11 @@
+
+(()=>{
+let name="Osman";
+
+const topla = (a,b) => a+b;
+console.log(topla(6,4))
+
+console.log(name)
+
+})()
+//topla(5,8)
