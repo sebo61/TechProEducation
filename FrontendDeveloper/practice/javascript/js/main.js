dbf218d0c4c9dba478b12ea58180c97b2e1f5e8e
Sebahattin KÖSE <sebo61@gmail.com>
2023-12-10 00:00:47 +0300

    101223

---
+++ FrontendDeveloper/practice/javascript/js/main.js
@@ -0,0 +1,7 @@
+import { square,pi } from "./utils.js";
+
+console.log(square(5));
+
+console.log(pi);
+
+
---
+++ FrontendDeveloper/practice/javascript/js/utils.js
@@ -0,0 +1,8 @@
+function square(x) {
+    return x*x;
+
+}
+
+const pi=3.1415;
+
+export { square,pi};
